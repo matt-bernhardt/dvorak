29db96f6fea40e1544ecf7e766863229f3309cf9
Matt Bernhardt <mjbernha@mit.edu>
2020-03-31 16:40:36 -0400

    Adds loading of parent theme styles per WP docs

--- functions.php
+++ functions.php
@@ -14,3 +14,25 @@
 if ( ! isset( $content_width ) ) {
 	$content_width = 1200; /* pixels */
 }
+
+/**
+ * Enqueue stylesheets
+ */
+function dvorak_enqueue_styles() {
+
+	$parent_style = 'twentynineteen-style';
+
+	wp_enqueue_style(
+		$parent_style,
+		get_template_directory_uri() . '/style.css',
+		'',
+		wp_get_theme()->get( 'Version' )
+	);
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'dvorak_enqueue_styles' );
